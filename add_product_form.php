58d9780c67fda702b6081c139a0d5b9dc233f71d
Jake Staab <jjstaab@mail.fhsu.edu>
2021-02-16 12:27:22 -0600

    first commit

---
+++ add_product_form.php
@@ -0,0 +1,51 @@
+<?php
+    require('database.php');
+    $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
+    $description = filter_input(INPUT_GET, "description", FILTER_SANITIZE_STRING);
+    $category_id = filter_input(INPUT_POST, "category_id", FILTER_VALIDATE_INT);
+
+        $query = 'SELECT * FROM categories
+                        ORDER BY categoryID';
+        $statement = $db->prepare($query);
+        $statement->execute();
+        $categories = $statement->fetchAll();
+        $statement->closeCursor();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ToDo LIst</title>
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+    <header><h1>Product Manager</h1></header>
+    <main>
+        <h1>Add Product</h1>
+        <section>
+            <div class="additem">
+            <form action="add_product.php" method="POST">
+
+                <label>Category:</label>
+                <select name="category_id">
+                <?php foreach ($categories as $category) { ?>
+                    <option value="<?php echo $category['categoryID']; ?>">
+                        <?php echo $category['categoryName']; ?>
+                    </option>
+                <?php } ?>
+                <label><div class="addlabel">Add Item</div></label>
+                <input type="hidden" name="itemnum" value="<?php echo $itemnum ?>">
+                <div>
+                    <input type="text" id="newitems" name="title" placeholder="Title" required><br>
+                    <input type="text" id="newitems" name="description" placeholder="Description" required>
+                </div>
+                <button class="add">Add Item</button>
+            </form>
+            </div>
+        </section>
+        <p><a href="index.php">View ToDo List</a></p>
+    </main>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+    require('database.php');
+    $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
+    $description = filter_input(INPUT_GET, "description", FILTER_SANITIZE_STRING);
+
+        $query = 'SELECT * FROM todoitems
+                        ORDER BY ItemNum ASC';
+        $statement = $db->prepare($query);
+        $statement->bindValue(':title', $title);
+        $statement->bindValue(':description', $description);
+        $statement->execute();
+        $results = $statement->fetchAll();
+        $statement->closeCursor();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ToDo LIst</title>
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+    <main>
+
+        <?php if(!empty($results)) { ?>
+            <section>
+                <table>
+                    <tr><div class="listlabel">ToDo List</div></tr>
+                    <?php foreach($results as $result) {
+                        $itemnum = $result['ItemNum'];
+                        $title = $result['Title'];
+                        $description = $result['Description'];
+                    ?>
+                    <tr>
+                        <td><span class="title"><?php echo $title; ?></span>
+                            <div><?php echo $description; ?></div></td>
+                        <td><form action="delete_product.php" method="POST">
+                            <input type="hidden" name="itemnum" value="<?php echo $itemnum ?>">
+                            <button class="delete">&#10006</button>
+                        </form></td>
+                    </tr>
+                    <?php } ?>
+                </table>
+            </section>
+        <?php } else { ?>
+            <p>There are no items on the ToDo List.</p>
+        <?php } ?>
+
+        <p><a href="add_product_form.php">Add Product</a></p>
+
+    </main>
+</body>
+</html>
