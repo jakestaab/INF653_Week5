17f6262c8c249aba4b4173b94ed99e58b7800125
Jake Staab <jjstaab@mail.fhsu.edu>
2021-02-20 18:45:17 -0600

    sweet sassy molassey

--- view/add_item_form.php
+++ view/add_item_form.php
@@ -4,7 +4,7 @@
         <div class="tbl">
         <h2>Add Item</h2>
         <section>
-            <div class="additem">
+            <div class="">
                 <form action="index.php" method="POST">
                     <input type="hidden" name="action" value="add_item">
                     <div class="selectCategory">
@@ -17,11 +17,11 @@
                     <?php } ?>
                     </select>
                     </div>
-                    <!-- <label><div class="addlabel">Add Item</div></label>
-                    <input type="hidden" name="itemnum" value="<?php echo $itemnum ?>"> -->
                     <div>
-                        <input type="text" id="newitems" name="title" placeholder="Title" required /><br>
-                        <input type="text" id="newitems" name="description" placeholder="Description" required />
+                        <input type="text" id="newitems" name="title"
+                            placeholder="Title" style="width: 275px;" required /><br>
+                        <input type="text" id="newitems" name="description"
+                            placeholder="Description" style="width: 275px;" required />
                     </div>
                     <label>&nbsp;</label>
                     <input class ="categoryButton" type="submit" value="Add Item" />
